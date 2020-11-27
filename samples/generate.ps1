9e12374d2eb2be3aba0247e6c20a10132d2fc26c
Kane Armstrong <kane.armstrong@outlook.com>
2020-11-27 21:01:56 +1300

    Update sample invocation

--- samples/generate.ps1
+++ samples/generate.ps1
@@ -1,3 +1,3 @@
 # This assumes the executable is in the same folder as this script
 
-.\Armsoft.RestApiFromSqlSchema.CLI.exe -c "Server=<server>;Initial Catalog=AdventureWorks2016CTP3;Persist Security Info=False;User ID=<user>;Password=<password>;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;" -s "AdventureWorksApi" -d "c:\temp\generated code\AdventureWorksApi2" -p "AdventureWorksApi
+.\apigen.exe -c "Server=<server>;Initial Catalog=AdventureWorks2016CTP3;Persist Security Info=False;User ID=<user>;Password=<password>;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;" -s "AdventureWorksApi" -o "c:\temp\generated code\AdventureWorksApi2" -p "AdventureWorksApi
