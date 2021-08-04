60b9651ccaa46ea584ece493ac89ef1bf40a9cd1
Kane Armstrong <kane.armstrong@outlook.com>
2021-08-04 20:48:44 +1200

    Add incomplete test to cover table representation

---
+++ test/SchemaExplorer.SqlServer.Tests/Experimental/SqlServerSchemaExplorerSpec/TableTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using SchemaExplorer.SqlServer.Tests.Infrastructure;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SchemaExplorer.SqlServer.Tests.Experimental.SqlServerSchemaExplorerSpec
+{
+    public class TableTests
+    {
+        [Theory]
+        [MemberData(nameof(ExpectedTableToColumnNameMappings))]
+        public async Task Explorer_captures_table_columns_correctly(string schemaName, SchemaExplorer.Experimental.Table fixture)
+        {
+            var sut = new SqlServerSchemaExplorerBuilder().Build();
+            var db = await sut.ExploreDatabase(CancellationToken.None);
+            var table = db.Schema.First(x => x.Name == schemaName).Tables.First(x => x.Name == fixture.Name);
+            table.Columns.Should().BeEquivalentTo(fixture.Columns);
+        }
+
+        public static IEnumerable<object[]> ExpectedTableToColumnNameMappings
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    "dbo",
+                    new SchemaExplorer.Experimental.Table
+                    {
+                        Name = "JoinWithMe",
+                        Columns = new []
+                        {
+                            new SchemaExplorer.Experimental.Column
+                            {
+                                Name = "Id",
+                                AllowsNulls = false,
+                                DataType = "uniqueidentifier",
+                                Order = 1
+                            }
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
