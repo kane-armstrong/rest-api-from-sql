49bf805cddbfb919f08363e2c7387332c02d27d8
Kane Armstrong <kane.armstrong@outlook.com>
2021-08-04 16:09:11 +1200

    Add builder for tests to new up explorer

---
+++ test/SchemaExplorer.SqlServer.Tests/Infrastructure/SqlServerSchemaExplorerBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Options;
+using SchemaExplorer.Experimental;
+
+namespace SchemaExplorer.SqlServer.Tests.Infrastructure
+{
+    public class SqlServerSchemaExplorerBuilder
+    {
+        public SqlServerSchemaExplorer Build()
+        {
+            return new(Options.Create(new SqlServerSchemaExplorerOptions
+            {
+                ConnectionString = "Server=.;Initial Catalog=SchemaExplorerTests;Persist Security Info=False;User Name=TestRunner;Password=TestRunner123;"
+            }));
+        }
+    }
+}
