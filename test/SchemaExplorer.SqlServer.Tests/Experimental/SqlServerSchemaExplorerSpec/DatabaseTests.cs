ce99c6c9c9932f9b224e953f5e7a831b8ba338c2
Kane Armstrong <kane.armstrong@outlook.com>
2021-08-04 20:07:07 +1200

    Add tests to cover database/schema

---
+++ test/SchemaExplorer.SqlServer.Tests/Experimental/SqlServerSchemaExplorerSpec/DatabaseTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using SchemaExplorer.SqlServer.Tests.Infrastructure;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SchemaExplorer.SqlServer.Tests.Experimental.SqlServerSchemaExplorerSpec
+{
+    public class DatabaseTests
+    {
+        [Fact]
+        public async Task Explorer_produces_the_correct_database_name()
+        {
+            const string databaseName = "SchemaExplorerTests";
+
+            var sut = new SqlServerSchemaExplorerBuilder().Build();
+
+            var db = await sut.ExploreDatabase(CancellationToken.None);
+            db.Name.Should().Be(databaseName);
+        }
+
+        [Fact]
+        public async Task Explorer_yields_the_expected_schema()
+        {
+            const string first = "dbo";
+            const string second = "Demo";
+
+            var sut = new SqlServerSchemaExplorerBuilder().Build();
+
+            var db = await sut.ExploreDatabase(CancellationToken.None);
+            db.Schema.Should().Contain(x => x.Name == first);
+            db.Schema.Should().Contain(x => x.Name == second);
+        }
+
+        [Fact]
+        public async Task Explorer_returns_the_correct_number_of_schema()
+        {
+            const int expectedSchemaCount = 2;
+
+            var sut = new SqlServerSchemaExplorerBuilder().Build();
+
+            var db = await sut.ExploreDatabase(CancellationToken.None);
+            db.Schema.Count().Should().Be(expectedSchemaCount);
+        }
+    }
+}
---
+++ test/SchemaExplorer.SqlServer.Tests/Experimental/SqlServerSchemaExplorerSpec/SchemaTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using SchemaExplorer.SqlServer.Tests.Infrastructure;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SchemaExplorer.SqlServer.Tests.Experimental.SqlServerSchemaExplorerSpec
+{
+    public class SchemaTests
+    {
+        private const string DboSchemaName = "dbo";
+        private const string DemoSchemaName = "Demo";
+
+        [Fact]
+        public async Task Explorer_produces_the_correct_schema_name_for_dbo()
+        {
+            var sut = new SqlServerSchemaExplorerBuilder().Build();
+
+            var db = await sut.ExploreDatabase(CancellationToken.None);
+            db.Schema.Should().Contain(x => x.Name == DboSchemaName);
+        }
+
+        [Theory]
+        [InlineData("JoinWithMe")]
+        [InlineData("WithCheckConstraint")]
+        [InlineData("WithDefaultConstraint")]
+        [InlineData("WithForeignKeyConstraint")]
+        [InlineData("WithFourPrimaryKeys")]
+        [InlineData("WithNoPrimaryKey")]
+        [InlineData("WithOnePrimaryKey")]
+        [InlineData("WithTwoPrimaryKeys")]
+        [InlineData("WithUniqueConstraint")]
+        public async Task Explorer_yields_the_expected_tables_for_dbo(string tableName)
+        {
+            var explorer = new SqlServerSchemaExplorerBuilder().Build();
+
+            var db = await explorer.ExploreDatabase(CancellationToken.None);
+            var sut = db.Schema.Single(x => x.Name == DboSchemaName);
+            sut.Tables.Should().Contain(x => x.Name == tableName);
+        }
+
+        [Fact]
+        public async Task Explorer_produces_the_correct_schema_name_for_Demo()
+        {
+            var sut = new SqlServerSchemaExplorerBuilder().Build();
+
+            var db = await sut.ExploreDatabase(CancellationToken.None);
+            db.Schema.Should().Contain(x => x.Name == DemoSchemaName);
+        }
+
+        [Theory]
+        [InlineData("WithAllNotNullDataTypes")]
+        [InlineData("WithAllNullDataTypes")]
+        public async Task Explorer_yields_the_expected_tables_for_Demo(string tableName)
+        {
+            var explorer = new SqlServerSchemaExplorerBuilder().Build();
+
+            var db = await explorer.ExploreDatabase(CancellationToken.None);
+            var sut = db.Schema.Single(x => x.Name == DemoSchemaName);
+            sut.Tables.Should().Contain(x => x.Name == tableName);
+        }
+    }
+}
